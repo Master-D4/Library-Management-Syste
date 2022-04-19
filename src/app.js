387841aabf24d7bdf03ff25cbee0c18259f1a650
Master-D4 <dinethdisanayaka11@gmail.com>
2022-04-20 00:33:41 +0530

    2nd part

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("Hello YouTube");
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import logger from "./utils/logger";
+
+const app = express();
+const PORT = process.env.PORT || "8050";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb"}));
+
+app.get("/", (req, res, next) => {
+    res.send("<h2> 📚Library Management System API </h2>");
+    next();
+});
+
+app.listen(PORT, () => {
+    //logger.info("This is testing");
+    //logger.error("This is Error");
+    //logger.warn("This is Warning");
+    logger.info("🚀 Server is up and running on PORT"); 
+});
